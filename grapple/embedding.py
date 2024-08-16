b1d1c28d995e8d4ee660856aa966cb6f6fb1c32b
Will Bradley <williambbradley@gmail.com>
2024-08-16 08:32:10 -0600

    updates

---
+++ grapple/embedding.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+from typing import List
+from uuid import UUID
+
+from pydantic import BaseModel
+from scipy.spatial.distance import cosine
+
+Vector = List[float]
+
+
+class Embedding(BaseModel):
+    uuid: UUID
+    text: str
+    model: str
+    vector: List[float]
+
+
+@dataclass
+class EmbeddingWithDistance:
+    embedding: Embedding
+    distance: float
+
+
+def make_embedding_with_distance(
+    query: Vector, embedding: Embedding
+) -> EmbeddingWithDistance:
+    return EmbeddingWithDistance(
+        embedding=embedding, distance=cosine(embedding.vector, query)
+    )
+
+
+_embeddings: List[Embedding] = []
+
+
+def fetch_all_embeddings(cursor: Cursor) -> List[Embedding]:
+    # Cached once per process.
+    if _embeddings:
+        return _embeddings
+
+    with Timer("gather all embeddings"):
+        cursor.execute(
+            """
+                SELECT uuid, text, model, vector
+                FROM embedding
+                ORDER BY created_at
+            """
+        )
+        for row in cursor:
+            _embeddings.append(Embedding.parse_obj(row))
+        return _embeddings
+
+
+def get_k_nearest_embeddings(
+    cursor: Cursor, query: Vector, top_n: int
+) -> List[EmbeddingWithDistance]:
+    embeddings = fetch_all_embeddings(cursor)
+    with Timer("calculating cosine distances"):
+        embeddings_with_distance = [
+            EmbeddingWithDistance(embedding=x, distance=cosine(x.vector, query))
+            for x in embeddings
+        ]
+    embeddings_with_distance.sort(key=lambda x: x.distance)
+    return embeddings_with_distance[:top_n]
